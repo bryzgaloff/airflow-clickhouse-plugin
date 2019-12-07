9bce5f4ac54dddb381a7d6cb08034ddcef3e73aa
Anton Bryzgalov <anton.bryzgalov@annalect.omd.ru>
2019-12-08 01:16:31 +0300

    clickhouse_plugin/hooks/clickhouse_hook: refactoring

--- clickhouse_plugin/hooks/clickhouse_hook.py
+++ clickhouse_plugin/hooks/clickhouse_hook.py
@@ -1,80 +1,82 @@
-from contextlib import AbstractContextManager
+from typing import *
 
 from airflow.hooks.base_hook import BaseHook
 from clickhouse_driver import Client
 
 
 class ClickHouseHook(BaseHook):
-    conn_name_attr = 'clickhouse_conn_id'
-    default_conn_name = 'clickhouse_default'
+    _DEFAULT_CONN_ID = 'clickhouse_default'
 
-    def __init__(self, *args, **kwargs):
-        if len(args) == 1:
-            setattr(self, self.conn_name_attr, args[0])
-        elif self.conn_name_attr not in kwargs:
-            setattr(self, self.conn_name_attr, self.default_conn_name)
-        else:
-            setattr(self, self.conn_name_attr, kwargs[self.conn_name_attr])
-        self.database = kwargs.pop("database", None)
+    def __init__(
+            self,
+            clickhouse_conn_id: str = _DEFAULT_CONN_ID,
+            database: str = None,
+    ):
+        super().__init__(source=None)
+        self.clickhouse_conn_id = clickhouse_conn_id
+        self.database = database
 
     def get_conn(self) -> Client:
-        conn = self.get_connection(getattr(self, self.conn_name_attr))
-
-        conn_config = {
-            "user": conn.login or 'default',
-            "password": conn.password or '',
-            "host": conn.host or 'localhost',
-            "database": self.database or conn.schema or 'default'
-        }
-
-        if not conn.port:
-            conn_config["port"] = 9000
-        else:
-            conn_config["port"] = int(conn.port)
-
-        connection = ClickHouseHook.create_connection(conn_config)
-        return connection
+        conn = self.get_connection(self.clickhouse_conn_id)
+        return ClickHouseHook.create_connection(
+            host=conn.host or 'localhost',
+            port=int(conn.port) if conn.port else 9000,
+            user=conn.login or 'default',
+            password=conn.password or '',
+            database=self.database or conn.schema or 'default',
+        )
 
     @classmethod
-    def create_connection(cls, config: dict) -> Client:
-        return Client(**config)
+    def create_connection(
+            cls, host: str, port: int, database: str, user: str, password: str
+    ) -> Client:
+        return Client(host, port, database, user, password)
 
-    def get_records(self, sql, parameters=None):
+    def get_records(self, sql: str, parameters: dict = None) -> List[Tuple]:
+        with disconnecting(self.get_conn()) as client:
+            return client.execute(sql, params=parameters)
+
+    def get_first(self, sql: str, parameters: dict = None) -> Tuple:
+        with disconnecting(self.get_conn()) as client:
+            return next(client.execute_iter(sql, params=parameters))
+
+    def get_pandas_df(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def run(
+            self,
+            sql: str,
+            parameters: Union[dict, list, tuple, Generator] = None,
+    ) -> Any:
         with disconnecting(self.get_conn()) as conn:
             return conn.execute(sql, params=parameters)
 
-    def get_first(self, sql, parameters=None):
-        with disconnecting(self.get_conn()) as conn:
-            return next(conn.execute_iter(sql, params=parameters))
 
-    def get_pandas_df(self, sql, parameters=None):
-        raise NotImplementedError()
-
-    def run(self, sql, parameters=None):
-        with disconnecting(self.get_conn()) as conn:
-            return conn.execute(sql, params=parameters)
+_InnerT = TypeVar('_InnerT')
 
 
-class disconnecting(AbstractContextManager):
-    """Context to automatically close something at the end of a block.
+class disconnecting(ContextManager, Generic[_InnerT]):
+    """Context to automatically disconnects something at the end of a block.
 
     Code like this:
 
-        with closing(<module>.open(<arguments>)) as f:
-            <block>
+    >>> with disconnecting(<module>.open(<arguments>)) as f:
+    >>>     <block>
 
     is equivalent to this:
 
-        f = <module>.open(<arguments>)
-        try:
-            <block>
-        finally:
-            f.close()
-
+    >>> f = <module>.open(<arguments>)
+    >>> try:
+    >>>     <block>
+    >>> finally:
+    >>>     f.disconnect()
     """
-    def __init__(self, thing):
-        self.thing = thing
-    def __enter__(self):
-        return self.thing
-    def __exit__(self, *exc_info):
-        self.thing.disconnect()
+
+    def __init__(self, thing: _InnerT):
+        self._thing = thing
+
+    def __enter__(self) -> _InnerT:
+        return self._thing
+
+    def __exit__(self, *exc_info) -> None:
+        self._thing.disconnect()
