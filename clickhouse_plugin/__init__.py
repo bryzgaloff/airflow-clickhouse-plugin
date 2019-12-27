3b4fc63351ac1fdf8e7813a2b2839e80e19efe7c
Anton Bryzgalov <anton.bryzgalov@samsungnext.com>
2019-12-27 19:19:14 +0300

    clickhouse_plugin/operators/clickhouse_operator: implemented / WXDATA-128

--- clickhouse_plugin/__init__.py
+++ clickhouse_plugin/__init__.py
@@ -5,9 +5,10 @@ clickhouse_plugin
 
 from airflow.plugins_manager import AirflowPlugin
 from .hooks.clickhouse_hook import ClickHouseHook
+from .operators.clickhouse_operator import ClickHouseOperator
 
 
 class ClickHousePlugin(AirflowPlugin):
     name = 'clickhouse_plugin'
-    operators = []
+    operators = [ClickHouseOperator]
     hooks = [ClickHouseHook]
