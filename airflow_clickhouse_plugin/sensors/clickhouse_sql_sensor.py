16ecee3f136b462e42d4520216af72512493358c
Alexander Chashnikov <alexander.chashnikov@betpawa.com>
2020-05-25 17:59:29 +0300

    poke method implemented for backward compatibility

--- airflow_clickhouse_plugin/sensors/clickhouse_sql_sensor.py
+++ airflow_clickhouse_plugin/sensors/clickhouse_sql_sensor.py
@@ -1,3 +1,4 @@
+from airflow.exceptions import AirflowException
 from airflow.sensors.sql_sensor import SqlSensor
 from airflow_clickhouse_plugin.hooks import clickhouse_hook
 
@@ -5,19 +6,51 @@ from airflow_clickhouse_plugin.hooks import clickhouse_hook
 class ClickHouseSqlSensor(SqlSensor):
     def __init__(
         self,
-        clickhouse_conn_id: str = "clickhouse_default",
-        database=None,
+        clickhouse_conn_id: str = 'clickhouse_default',
+        database: str = None,
         *args,
-        **kwargs
+        **kwargs,
     ):
         super(ClickHouseSqlSensor, self).__init__(conn_id=None, *args, **kwargs)
         self._conn_id = clickhouse_conn_id
         self._database = database
 
-    def _get_hook(self):
-        return clickhouse_hook.ClickHouseHook(
+    def poke(self, context):
+        hook = clickhouse_hook.ClickHouseHook(
             clickhouse_conn_id=self._conn_id, database=self._database,
         )
 
-    def poke(self, context):
-        return super(ClickHouseSqlSensor, self).poke(context)
+        self.log.info("Poking: %s (with parameters %s)", self.sql, self.parameters)
+        records = hook.get_records(self.sql, self.parameters)
+        if not records:
+            if self.fail_on_empty:
+                raise AirflowException(
+                    "No rows returned, raising as per fail_on_empty flag"
+                )
+            else:
+                return False
+        first_cell = records[0][0]
+        if self.failure is not None:
+            if callable(self.failure):
+                if self.failure(first_cell):
+                    raise AirflowException(
+                        "Failure criteria met. self.failure({}) returned True".format(
+                            first_cell
+                        )
+                    )
+            else:
+                raise AirflowException(
+                    "self.failure is present, but not callable -> {}".format(
+                        self.success
+                    )
+                )
+        if self.success is not None:
+            if callable(self.success):
+                return self.success(first_cell)
+            else:
+                raise AirflowException(
+                    "self.success is present, but not callable -> {}".format(
+                        self.success
+                    )
+                )
+        return str(first_cell) not in ("0", "")
