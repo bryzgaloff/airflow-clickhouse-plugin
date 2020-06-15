86fbc1d9f1348cca38897440280f563bca213d92
Anton Bryzgalov <tony.bryzgaloff@gmail.com>
2020-06-15 21:53:46 +0300

    tests/unit/sensor: tests fixes due to review on Airflow 1.10.10

--- tests/unit/test_sql_sensor.py
+++ tests/unit/test_sql_sensor.py
@@ -2,7 +2,7 @@ import unittest
 from unittest import mock
 
 from airflow.exceptions import AirflowException
-from airflow.operators.sensors import SqlSensor
+from airflow.sensors.sql_sensor import SqlSensor
 
 from airflow_clickhouse_plugin import ClickHouseSqlSensor
 
@@ -132,20 +132,22 @@ class ClickHouseLegacySqlSensorTestCase(unittest.TestCase):
         'airflow_clickhouse_plugin.ClickHouseSqlSensor'
             '._ClickHouseSqlSensor__poke_clickhouse',
     )
-    @mock.patch(
-        'airflow_clickhouse_plugin.sensors.clickhouse_sql_sensor.SqlSensor',
-    )
+    @mock.patch('airflow.sensors.sql_sensor.SqlSensor.poke')
+    @mock.patch('airflow.sensors.sql_sensor.SqlSensor._get_hook', create=True)
     def test_get_hook_missing(
             self,
-            sql_sensor_mock: mock.MagicMock,
+            sql_sensor_get_hook_mock: mock.MagicMock,
+            sql_sensor_poke_mock: mock.MagicMock,
             clickhouse_sensor_poke_mock: mock.MagicMock,
     ):
         """ Test for case when ``SqlSensor._get_hook`` method is present. """
         op = ClickHouseSqlSensor(task_id='_', sql='')
-        delattr(sql_sensor_mock, '_get_hook')
+        del SqlSensor._get_hook
         op.poke(context=None)
-        sql_sensor_mock.poke.assert_not_called()
+        sql_sensor_poke_mock.assert_not_called()
         clickhouse_sensor_poke_mock.assert_called_once_with()
+        # line below prevents AttributeError on patch exit
+        SqlSensor._get_hook = sql_sensor_get_hook_mock
 
 
 if __name__ == '__main__':
