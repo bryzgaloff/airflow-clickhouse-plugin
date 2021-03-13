f25a049744b0ef87152211a6fd5994543083d012
Anton Bryzgalov <tony.bryzgaloff@gmail.com>
2021-03-13 14:25:04 +0300

    setup.py: refactoring + requirements added

--- setup.py
+++ setup.py
@@ -1,35 +1,33 @@
 #!/usr/bin/env python3
 
-import os
+import pathlib
 from setuptools import setup, find_packages
 
-# get key package details from airflow_clickhouse_plugin/__version__.py
-about = {}
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'airflow_clickhouse_plugin', '__version__.py')) as f:
-    exec(f.read(), about)
+import airflow_clickhouse_plugin.__version__ as about
 
-# load the README file and use it as the long_description for PyPI
-with open(os.path.join(here, 'README.md'), 'r') as f:
-    readme = f.read()
+here = pathlib.Path(__file__).parent.absolute()
+with open(here / 'README.md') as readme_file:
+    long_description = readme_file.read()
+with open(here / 'requirements.txt') as requirements_file:
+    install_requires = [
+        requirement_line.rstrip('\n')
+        for requirement_line in requirements_file
+    ]
 
 setup(
-    name=about['__title__'],
-    description=about['__description__'],
-    long_description=readme,
+    name=about.__title__,
+    description=about.__description__,
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    version=about['__version__'],
-    author=about['__author__'],
-    author_email=about['__author_email__'],
-    url=about['__url__'],
+    version=about.__version__,
+    author=about.__author__,
+    author_email=about.__author_email__,
+    url=about.__url__,
     packages=find_packages(exclude=('tests', 'tests.*')),
     include_package_data=True,
     python_requires=">=3.6.*",
-    install_requires=[
-        'clickhouse-driver~=0.1.3',
-        'apache-airflow~=2.0.1',
-    ],
-    license=about['__license__'],
+    install_requires=install_requires,
+    license=about.__license__,
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
