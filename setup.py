063aebfea3d945a41d678962651b5c297813c036
gkarg <70892175+gkarg@users.noreply.github.com>
2021-10-27 19:52:26 +0300

    Apply suggestion
    Co-authored-by: Anton Bryzgalov <tony.bryzgaloff+github@gmail.com>

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     python_requires=">=3.6.*",
     install_requires=install_requires,
     extras_require={
-        'pandas': ['pandas~=1.1.0'],
+        'pandas': ['apache-airflow[pandas]>=2.0.0,<2.3.0'],
     },
     license=about.__license__,
     zip_safe=False,
