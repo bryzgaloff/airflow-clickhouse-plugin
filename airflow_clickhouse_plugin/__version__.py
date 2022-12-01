bf3070564e7c6e3e941bd8ce8a1d1f1e75a19df5
Anton Bryzgalov <tony.bryzgaloff@gmail.com>
2022-12-01 15:46:54 +0300

    Version bumped to 0.9.1, PR #54
    apache-airflow-providers-common-sql added as a dependency.

--- airflow_clickhouse_plugin/__version__.py
+++ airflow_clickhouse_plugin/__version__.py
@@ -9,7 +9,7 @@ __title__ = 'airflow-clickhouse-plugin'
 __description__ = \
     'airflow-clickhouse-plugin - ' \
     'Airflow plugin to execute ClickHouse commands and queries'
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 __url__ = 'https://github.com/bryzgaloff/airflow-clickhouse-plugin'
 __license__ = 'MIT License'
 __author__ = 'Anton Bryzgalov'
