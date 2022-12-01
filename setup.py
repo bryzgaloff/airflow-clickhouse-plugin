e56994fa98c4223a05fea123aa2ed5df26417bff
Anton Bryzgalov <tony.bryzgaloff@gmail.com>
2022-12-01 16:05:25 +0300

    setup: Python 3.11 added to the list

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.8',
