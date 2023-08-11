64de258dbbf24b275be5290b0be23b9bb2369fda
Anton Bryzgalov <tony.bryzgaloff@gmail.com>
2023-08-11 15:52:19 +0200

    pyproject.toml: replaces setup.py and __version__.py
    pyproject is a modern way for packages definition

--- airflow_clickhouse_plugin/__version__.py
+++
@@ -1,9 +0,0 @@
-__title__ = 'airflow-clickhouse-plugin'
-__description__ = \
-    'airflow-clickhouse-plugin — ' \
-    'Airflow plugin to execute ClickHouse commands and queries'
-__version__ = '0.11.0'
-__url__ = 'https://github.com/bryzgaloff/airflow-clickhouse-plugin'
-__license__ = 'MIT License'
-__author__ = 'Anton Bryzgalov'
-__author_email__ = 'tony.bryzgaloff@gmail.com'
