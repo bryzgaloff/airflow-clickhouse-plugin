60954d26e3f6a64a890aedf4c306bf3e839a3544
Anton Bryzgalov <tony.bryzgaloff@gmail.com>
2023-08-24 20:56:25 +0200

    tests/integration/airflow_env_conn.py: os.environ.setdefault

--- tests/integration/airflow_env_conn.py
+++ tests/integration/airflow_env_conn.py
@@ -8,11 +8,12 @@ from airflow_clickhouse_plugin.hooks.clickhouse import ClickHouseHook, default_c
 class ClickHouseConnectionEnvironTestCaseBase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls._env_patcher = mock.patch.dict(
-            os.environ,
-            {f'AIRFLOW_CONN_{default_conn_name.upper()}': 'clickhouse://localhost'},
-        )
+        cls._env_patcher = mock.patch.dict(os.environ)
         cls._env_patcher.start()
+        os.environ.setdefault(
+            f'AIRFLOW_CONN_{default_conn_name.upper()}',
+            'clickhouse://localhost',
+        )
 
     @classmethod
     def tearDownClass(cls) -> None:
