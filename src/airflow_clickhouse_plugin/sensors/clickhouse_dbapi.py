f1cac816f9ba1043c5d707b7a8411deccd923c18
Anton Bryzgalov <tony.bryzgaloff@gmail.com>
2023-08-28 17:57:17 +0200

    src/airflow_clickhouse_plugin/sensors/clickhouse_dbapi.py: optimize imports

--- src/airflow_clickhouse_plugin/sensors/clickhouse_dbapi.py
+++ src/airflow_clickhouse_plugin/sensors/clickhouse_dbapi.py
@@ -1,4 +1,5 @@
 from airflow.providers.common.sql.sensors.sql import SqlSensor
+
 from airflow_clickhouse_plugin.hooks.clickhouse_dbapi import ClickHouseDbApiHook
 from airflow_clickhouse_plugin.operators.clickhouse_dbapi import \
     ClickHouseDbApiHookMixin
